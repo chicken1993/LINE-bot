74ebac7fc15f9be9e247369ac543366770799bca
株式会社もっちもっち <chicken.199305@gmail.com>
2026-04-11 16:32:35 +0900

    force run

--- app.py
+++ app.py
@@ -42,6 +42,7 @@ def handle_message(event):
 
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
