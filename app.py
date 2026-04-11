326e86eacdfe51a0244715a3ec079b9503389f0c
株式会社もっちもっち <chicken.199305@gmail.com>
2026-04-11 16:29:40 +0900

    final version

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from linebot.v3.webhooks import MessageEvent, TextMessageContent
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    return "Hello from LINE bot!"
+
 CHANNEL_ACCESS_TOKEN = "Sw87dewMvMr62IdT9ajyxDiKS7QaN9+RJTftPuEAbTB5OFS2wU7J49MeHT71UlPXpsyWn9PaSfdMyN17C+fZxrujGyY7K53pBOYGDQxvXoBN1CFMpJWYY2bGlFcBdnBYxuYBmLWWhX0FbS20PdHsZQdB04t89/1O/w1cDnyilFU="
 CHANNEL_SECRET = "9893629cbabdb7270d0163b095bbe665"
 
