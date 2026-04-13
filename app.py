7a9497b0c387b582b396df6fcaa417f3888abe6b
株式会社もっちもっち <chicken.199305@gmail.com>
2026-04-13 23:06:41 +0900

    家計botデータ保存機能追加

--- app.py
+++ app.py
@@ -3,13 +3,32 @@ from linebot.v3 import WebhookHandler
 from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 import os
+from dotenv import load_dotenv
+import json
+
+# .env読み込み
+load_dotenv()
 
 app = Flask(__name__)
+
+FILE_NAME = "kakeibo.json"
+
+def load_data():
+    try:
+        with open(FILE_NAME, "r") as f:
+            return json.load(f)
+    except:
+        return []
+
+def save_data(data):
+    with open(FILE_NAME, "w") as f:
+        json.dump(data, f)
 
 @app.route("/")
 def home():
     return "Hello from LINE bot!"
 
+# 環境変数から取得
 CHANNEL_ACCESS_TOKEN = os.getenv("CHANNEL_ACCESS_TOKEN")
 CHANNEL_SECRET = os.getenv("CHANNEL_SECRET")
 
@@ -23,20 +42,49 @@ def callback():
 
     try:
         handler.handle(body, signature)
-    except:
+    except Exception as e:
+        print("エラー内容:", e)
         abort(400)
 
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    print("受信:", event.message.text)
+    text = event.message.text
+    print("受信:", text)
+
+    # データ読み込み
+    data = load_data()
+
+    if text == "合計":
+        total = sum(data)
+        reply_text = f"合計：{total}円"
+
+    else:
+        try:
+            name, price = text.split()
+            price = int(price)
+
+            # データ追加
+            data.append(price)
+
+            # 保存
+            save_data(data)
+
+            reply_text = f"{name} を {price}円で記録したよ！"
+
+        except:
+            reply_text = "『ラーメン 900』みたいに入力してね"
 
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
-                messages=[TextMessage(text="受け取ったよ！")]
+                messages=[TextMessage(text=reply_text)]
             )
         )
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
