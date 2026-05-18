75a54815a0e179828e5537b13c49f1b43757ae14
株式会社もっちもっち <chicken.199305@gmail.com>
2026-05-18 20:18:31 +0900

    update

--- app.py
+++ app.py
@@ -501,15 +501,20 @@ def create_graph(user_id):
 # ======================
 
 def detect_text_from_image(image_bytes):
+
     client = vision.ImageAnnotatorClient()
+
     image = vision.Image(content=image_bytes)
+
     response = client.text_detection(image=image)
+
     texts = response.text_annotations
 
     if not texts:
         return ""
 
     return texts[0].description
+
 
 
 def extract_receipt_info(text):
